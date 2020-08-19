c209d62100edc900e38f23339732f4a0985b4e01
Iridium <iridiumlegeartis@gmail.com>
2020-08-20 00:40:56 +0300

    beta

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var actionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 1
+        yellowView.alpha = 0.5
+        greenView.alpha = 0.5
+        actionButton.setTitle("NEXT1", for: .normal)
+        
     }
 
-
+    @IBAction func actionButtonPushed() {
+        if redView.alpha == 1 {
+            redView.alpha = 0.5
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.5
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.5
+            redView.alpha = 1
+        }
+    }
 }
 
