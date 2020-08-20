4e8f2d5daccd1401ec615b8eca07bcc217d320ec
Iridium <iridiumlegeartis@gmail.com>
2020-08-20 07:57:17 +0300

    Release

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -9,28 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var stackOfViews: UIStackView!
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var actionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.alpha = 1
-        yellowView.alpha = 0.5
-        greenView.alpha = 0.5
-        actionButton.setTitle("NEXT1", for: .normal)
-        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        actionButton.setTitle("START", for: .normal)
+//        redView.layer.cornerRadius = redView.layer.frame.height / 2
+//        greenView.layer.cornerRadius = greenView.layer.frame.height / 2
+//        yellowView.layer.cornerRadius = yellowView.layer.frame.height / 2
+//        print(redView.layer.cornerRadius, redView.layer.frame.height)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        redView.layer.cornerRadius = redView.layer.frame.height / 2
+        greenView.layer.cornerRadius = greenView.layer.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.layer.frame.height / 2
+        print(redView.layer.cornerRadius, redView.layer.frame.height)
     }
 
     @IBAction func actionButtonPushed() {
+        actionButton.setTitle("NEXT", for: .normal)
         if redView.alpha == 1 {
-            redView.alpha = 0.5
+            redView.alpha = 0.3
             yellowView.alpha = 1
         } else if yellowView.alpha == 1 {
-            yellowView.alpha = 0.5
+            yellowView.alpha = 0.3
             greenView.alpha = 1
         } else {
-            greenView.alpha = 0.5
+            greenView.alpha = 0.3
             redView.alpha = 1
         }
     }
